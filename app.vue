a7714c0451d5108727a1c2c0bc2d9c17cea17b33
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-03-21 15:55:45 +0100

    feat: Update app layout and add dark theme variables

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <main class="bg-primary-bg-color">
     <NuxtRouteAnnouncer />
     <!-- <NuxtWelcome /> -->
-    <h1 class="flex text-red-400">Hello World</h1>
-  </div>
+    <h1 class="flex text-primary-color">Hello World</h1>
+  </main>
 </template>
