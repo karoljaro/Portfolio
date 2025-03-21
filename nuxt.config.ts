6d421f6885ff424d135020633a76c71857d6751d
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-03-21 15:56:35 +0100

    feat: Update project configuration with new font support and additional dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,28 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/test-utils",
-    // "@nuxtjs/i18n",
-    // "@nuxtjs/seo",
-  ],
-  css: ['@/assets/css/globals.css'],
+  css: ["@/assets/css/globals.css"],
   vite: {
     plugins: [tailwindcss()],
   },
+  fonts: {
+    assets: {
+      prefix: '/_fonts/'
+    },
+    families: [
+      {
+        name: 'Open Sans',
+        provider: 'google',
+        display: "swap",
+      }
+    ],
+  },
+  devtools: { enabled: true },
+  modules: [  
+    "@nuxt/eslint", 
+    "@nuxt/test-utils", 
+    "@nuxt/fonts"
+    // "@nuxtjs/i18n",
+    // "@nuxtjs/seo",
+  ],
 });
