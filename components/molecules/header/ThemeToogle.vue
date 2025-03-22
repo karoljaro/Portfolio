29e88e90a7b823aa869352a76832e6bc0b997963
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-03-22 02:54:13 +0100

    feat: implement theme toggle functionality with improved dark mode handling

--- components/molecules/header/ThemeToogle.vue
+++ components/molecules/header/ThemeToogle.vue
@@ -1,10 +1,25 @@
 <template>
-  <AtomsHeaderBoxThemeToogle @click="() => console.log(isDark = !isDark)">
-    <Icon :name="isDark ? 'lucide:moon' : 'lucide:sun'" class="text-2xl" />
+  <AtomsHeaderBoxThemeToogle @click="toggleTheme">
+    <Icon
+      :name="mode == 'dark' ? 'lucide:moon' : 'lucide:sun'"
+      class="text-2xl"
+    />
   </AtomsHeaderBoxThemeToogle>
 </template>
 
 <script lang="ts" setup>
-const isDark = ref(true);
+const { system, store } = useColorMode({
+  selector: "html",
+  attribute: "data-theme",
+});
+
+const mode = computed(() =>
+  store.value === "auto" ? system.value : store.value,
+);
+
+const toggleTheme = () => {
+  store.value = store.value === "dark" ? "light" : "dark";
+};
+
 </script>
 <!-- lucide:sun lucide:moon -->
