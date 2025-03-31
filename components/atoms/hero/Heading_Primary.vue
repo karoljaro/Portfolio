0315403f16995844b6463a95852c981120be0d5f
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-03-31 23:08:03 +0200

    feat: improve Heading_Primary and Heading_Secondary components with enhanced responsive text sizing

--- components/atoms/hero/Heading_Primary.vue
+++ components/atoms/hero/Heading_Primary.vue
@@ -1,6 +1,6 @@
 <template>
   <h2
-    class="font-FiraCode text-wrap sm:text-nowrap  dark:text-primary-dark-color text-primary-light-color xs:text-6xl lg:text-9xl xl:text-9xl 2xl:text-10xl 3xl:text-11xl text-5xl !leading-none font-semibold -tracking-[3%] sm:text-8xl"
+    class="font-FiraCode dark:text-primary-dark-color text-primary-light-color xs:text-6xl lg:text-10xl 3xl:text-11xl text-5xl !leading-none font-semibold -tracking-[3%] text-wrap sm:text-8xl sm:text-nowrap"
   >
     <slot />
   </h2>
