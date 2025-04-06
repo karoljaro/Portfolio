456f1f0e005c0e0bbdb7fa2e2da9f8f8fd2b1bbd
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-07 01:43:54 +0200

    feat: enhance Button_Action component with improved styling and type prop

--- components/atoms/section_project/Button_Action.vue
+++ components/atoms/section_project/Button_Action.vue
@@ -1,19 +1,27 @@
 <template>
-  <NuxtLink :to="$props['to']" target="_blank" rel="noopener noreferrer">
-    <slot />
+  <NuxtLink
+    :to="$props['to']"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="font-FiraCode border-primary-border inline-flex items-center justify-between gap-x-2 rounded-full border bg-[#121212] p-4 text-left text-xl font-semibold text-[#F5F5F5] hover:bg-primary-border duration-300"
+    
+  >
+    <Icon :name="$props['type'] === 'GitHub' ? 'lucide:github' : 'lucide:radio'" class="text-2xl" />
+    {{ type }}
   </NuxtLink>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
   to: string;
+  type: "GitHub" | "Demo" | "Live";
 }>();
 
-const isInternalLink = (url: string) => {
-  return url.startsWith('/') || url.startsWith('#');
+const isInternalLink = (url: HTMLAnchorElement["href"]) => {
+  return url.startsWith("/") || url.startsWith("#");
 };
 
-if (isInternalLink(props['to'])) {
-  console.warn('Uwaga: przekazano link wewnętrzny, który nie powinien być otwierany w nowej karcie: ', props['to']);
+if (isInternalLink(props["to"])) {
+  console.warn("Uwaga: przekazano link wewnętrzny, który nie powinien być otwierany w nowej karcie: ", props["to"]);
 }
 </script>
