ac87549b21f5291f4a4f956c7f1945a09764ea67
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-14 16:18:43 +0200

    feat: add footer components including social buttons, credits, and person info

--- components/atoms/footer/Card_Credits.vue
+++ components/atoms/footer/Card_Credits.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 sm:p-8 rounded-5xl max-w-xl">
+  <div class="rounded-5xl border-primary-border h-fit max-w-xl space-y-2.5 border border-solid p-4 sm:p-8">
     <slot />
   </div>
 </template>
--- components/organism/Footer.vue
+++ components/organism/Footer.vue
@@ -1,5 +1,12 @@
 <template>
   <NuxtLayout name="section">
     <MoleculesSharedSectionLabel>Contact</MoleculesSharedSectionLabel>
+
+    <div class="flex flex-wrap items-center justify-between gap-x-8 gap-y-6">
+      <MoleculesFooterPersonInfo />
+      <MoleculesFooterCredits class="grow" />
+    </div>
+
+    <MoleculesFooterSocialButtons />
   </NuxtLayout>
 </template>
