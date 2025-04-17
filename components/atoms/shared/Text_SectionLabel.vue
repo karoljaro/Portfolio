97f725f04e7881ca48014ba1f80593f087294452
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-17 22:50:45 +0200

    fix: update font size for improved readability in Text_SectionLabel component

--- components/atoms/shared/Text_SectionLabel.vue
+++ components/atoms/shared/Text_SectionLabel.vue
@@ -1,3 +1,3 @@
 <template>
-  <p class="font-FiraCode text-nowrap flex text-primary-color text-2xl font-semibold sm:text-4.5xl">> <slot /></p>
+  <p class="font-FiraCode text-nowrap flex text-primary-color text-2.8xl font-semibold sm:text-5xl">> <slot /></p>
 </template>
