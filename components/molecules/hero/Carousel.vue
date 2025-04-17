d0cf552c8d819b5092d62553c4c14e2a258be878
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-17 19:21:51 +0200

    refactor: remove static tech list from AtomsHeroComponentCarousel for cleaner code

--- components/molecules/hero/Carousel.vue
+++ components/molecules/hero/Carousel.vue
@@ -2,26 +2,8 @@
   <div>
     <AtomsSharedLinePrimary />
     <div class="lg:py-4 md:py-2 py-0">
-      <AtomsHeroComponentCarousel :tech-name-list="staticTechList" />
+      <AtomsHeroComponentCarousel />
     </div>
     <AtomsSharedLinePrimary />
   </div>
 </template>
-
-<script setup lang="ts">
-const staticTechList = [
-  "Nuxt.js",
-  "Vue",
-  "Next.js",
-  "React",
-  "Tailwindcss",
-  "Javascript",
-  "Typescript",
-  "Bun.js",
-  "Angular",
-  "Elysia.js",
-  "TRPC",
-  "Docker",
-  "Prisma",
-];
-</script>
