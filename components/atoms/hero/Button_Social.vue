b22c19b365db792321c1d8afafdd59b96413e584
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-17 17:14:26 +0200

    fix: update Button_Social component styles and adjust spacing in SocialBtns component

--- components/atoms/hero/Button_Social.vue
+++ components/atoms/hero/Button_Social.vue
@@ -2,12 +2,12 @@
   <NuxtLink
     :to="$props.to"
     target="_blank"
-    class="border-primary-border bg-primary-bg font-OpenSans text-third-color flex items-center gap-2 rounded-full border px-4 py-2 text-xl leading-[130%] font-light capitalize italic sm:px-7 sm:py-3 sm:text-2xl 2xl:text-3xl hover:bg-primary-hover-bg duration-300"
+    rel="noopener noreferrer"
+    class="border-primary-border bg-primary-bg font-OpenSans text-primary-color dark:hover:bg-primary-hover-bg flex items-center gap-2 rounded-full border px-4 py-2 text-xl leading-[130%] font-light capitalize italic duration-300 select-none hover:bg-[#e9e9e9] sm:px-7 sm:py-3 sm:text-2xl 2xl:text-3xl"
   >
-    <slot name="link_icon" class="nav_icon" />
-    <span class="!text-second-color">
-      <slot />
-    </span>
+    <slot name="link_icon" />
+
+    <slot />
   </NuxtLink>
 </template>
 
@@ -16,9 +16,3 @@ defineProps<{
   to: string;
 }>();
 </script>
-
-<style lang="css" scoped>
-::v-deep(span) {
-  color: #f5f5f5;
-}
-</style>
