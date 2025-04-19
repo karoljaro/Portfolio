8173864fc63947bc4b8d03009fe5557d52be9ef2
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-19 15:21:55 +0200

    fix: remove unused code in Icon_CardIcon component

--- components/atoms/section_work/Icon_CardIcon.vue
+++ components/atoms/section_work/Icon_CardIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <Icon :name="iconName" class="text-primary-color text-6.5xl" />
+  <Icon :name="iconName" class="text-primary-color text-5xl md:text-6.5xl" />
 </template>
 
 <script setup lang="ts">
