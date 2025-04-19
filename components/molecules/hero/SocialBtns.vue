acbd82f13baf4835a1716acea977a337a5bcaa95
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-19 17:34:21 +0200

    refactor: streamline social buttons component to use dynamic data for GitHub and LinkedIn

--- components/molecules/hero/SocialBtns.vue
+++ components/molecules/hero/SocialBtns.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="flex gap-4">
-    <AtomsHeroButtonSocial to="https://github.com/karoljaro" class="shadow-md">
-      <template #link_icon>
-        <Icon name="static_Lucide:github" />
-      </template>
-      <template #default>
-        GitHub
-      </template>
-    </AtomsHeroButtonSocial>
+    <template v-for="({name, icon, url}, index) in socialInfo" :key="index">
+      <AtomsHeroButtonSocial :to="url" class="shadow-md">
+        <template #link_icon>
+          <Icon :name="icon" />
+        </template>
+        <template #default> {{ name }} </template>
+      </AtomsHeroButtonSocial>
+    </template>
   </div>
 </template>
+
+<script setup lang="ts">
+import type { IconName } from "~/shared/types/iconsNamesUnion";
+
+interface SocialInfo_DATA {
+  name: string;
+  icon: `static_Lucide:${IconName}`;
+  url: string;
+}
+
+const socialInfo: SocialInfo_DATA[] = [
+  {
+    name: "GitHub",
+    icon: "static_Lucide:github",
+    url: "https://github.com/karoljaro",
+  },
+  {
+    name: "Linkedin",
+    icon: "static_Lucide:linkedin",
+    url: "https://www.linkedin.com/in/karol-jaro%C5%84-474214361/",
+  },
+];
+</script>
