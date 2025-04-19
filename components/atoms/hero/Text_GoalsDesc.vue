f81d199e2e12d2fa4950d9a1bad1f8fb783352ac
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-19 17:42:11 +0200

    fix: adjust font size in Text_GoalsDesc component for improved readability

--- components/atoms/hero/Text_GoalsDesc.vue
+++ components/atoms/hero/Text_GoalsDesc.vue
@@ -1,6 +1,6 @@
 <template>
   <p
-    class="font-OpenSans text-third-color !leading-[130%] font-normal text-xl xs:text-2xl lg:text-2.8xl"
+    class="font-OpenSans text-third-color !leading-[130%] font-normal text-lg xs:text-2xl lg:text-2.8xl"
   >
     <slot />
   </p>
