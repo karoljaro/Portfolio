c90a1059a5e2b36650fed8419485a0883a5480c1
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-19 17:48:03 +0200

    fix: update font size in Text_Description component for better responsiveness

--- components/atoms/section_about/Text_Description.vue
+++ components/atoms/section_about/Text_Description.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="font-OpenSans text-second-color 3xl:text-3xl text-left text-base font-normal tracking-[4%] sm:text-xl md:text-2xl">
+  <p class="font-OpenSans text-second-color text-left text-base font-normal tracking-[4%] sm:text-xl md:text-2xl 3xl:text-2.8xl">
     <slot />
   </p>
 </template>
