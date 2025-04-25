f41ee1db1712f03b002413b46676e9bdfb855bcb
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-04-25 16:12:24 +0200

    refactor: enhance text processing and rendering for goals description with highlight support

--- components/atoms/hero/Text_GoalsDesc.vue
+++ components/atoms/hero/Text_GoalsDesc.vue
@@ -1,7 +1,23 @@
 <template>
-  <p
-    class="font-OpenSans text-third-color !leading-[130%] font-normal text-lg xs:text-2xl lg:text-2.8xl"
-  >
-    <slot />
+  <p class="font-OpenSans text-third-color xs:text-2xl lg:text-2.8xl text-lg !leading-[130%] font-normal">
+    <template v-for="(segment, index) in segments" :key="index">
+      <span v-if="segment.type === 'highlight'" class="text-primary-color font-semibold">
+        {{ segment.content }}
+      </span>
+      <template v-else>
+        {{ segment.content }}
+      </template>
+    </template>
   </p>
 </template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    segments?: TextSegment[];
+  }>(),
+  {
+    segments: () => [],
+  },
+);
+</script>
--- components/organism/Hero.vue
+++ components/organism/Hero.vue
@@ -10,14 +10,14 @@
 
       <div class="flex flex-wrap-reverse items-center justify-between gap-4">
         <!-- GoalsDesc może się zawijać -->
-        <MoleculesHeroGoalsDesc class="min-w-72 flex-1 [@media(max-width:1440px)]:hidden" />
+        <MoleculesHeroGoalsDesc :description="heroStore.goalsDescription" class="min-w-72 flex-1 [@media(max-width:1440px)]:hidden" />
 
         <!-- SecondaryHeading nie powinien się zawijać -->
         <MoleculesHeroSecondaryHeading class="flex-shrink-0 whitespace-nowrap" />
       </div>
 
       <div class="flex flex-wrap items-center justify-between gap-8 [@media(min-width:1441px)]:hidden">
-        <MoleculesHeroGoalsDesc class="flex-1 basis-1/2" />
+        <MoleculesHeroGoalsDesc :description="heroStore.goalsDescription" class="flex-1 basis-1/2" />
         <MoleculesHeroProjectBtn class="flex-1 basis-1/2" />
       </div>
 
@@ -26,3 +26,8 @@
     <MoleculesHeroCarousel />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { useHeroStore } from '~/stores/hero';
+const heroStore = useHeroStore();
+</script>
