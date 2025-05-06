9ef2a92ed550a4fb9df1938a7566c61529a26211
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-05-06 11:38:13 +0200

    feat: integrate GSAP ScrollSmoother and ScrollTrigger for enhanced scrolling effects

--- app.vue
+++ app.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <!-- <OrganismThePreloader /> -->
-    <main>
+    <main id="smooth-wrapper">
       <NuxtRouteAnnouncer />
-      <NuxtLayout>
+      <NuxtLayout id="smooth-content">
         <NuxtPage />
       </NuxtLayout>
     </main>
   </div>
 </template>
+
+<script setup lang="ts">
+const { $ScrollSmoother } = useNuxtApp();
+
+onMounted(() => {
+  $ScrollSmoother.create({
+    smooth: 1.5,
+    effects: true,
+    smoothTouch: 0.1,
+  });
+});
+</script>
--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -1,5 +1,12 @@
 import gsap from "gsap"
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default defineNuxtPlugin((nuxtApp) => {
+   if (import.meta.client) {
+      gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+   }
    nuxtApp.provide("gsap", gsap);
+   nuxtApp.provide("ScrollSmoother", ScrollSmoother);
+   nuxtApp.provide("ScrollTrigger", ScrollTrigger)
 })
